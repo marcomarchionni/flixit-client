e36544d3d79f2930c02c750111c32a51e026d070
Marco Marchionni <marco.marchionni@gmail.com>
2023-03-06 22:33:56 +0100

    Added "Related Movies" below Movie View

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import MovieCard from '../movie-card/movie-card';
 import MovieView  from '../movie-view/movie-view';
+import { findRelatedMovies } from '../../utils/utils';
 
 const MainView = () => {
 
     const [ movies, setMovies ] = useState([]);
+    const [ selectedMovie, setSelectedMovie ] = useState(null);
 
     useEffect(()=> {
         fetch("https://itflix-staging.herokuapp.com/movies")
@@ -15,11 +17,18 @@ const MainView = () => {
         .catch(err=>console.error(err))
     },[]);
 
-    const [ selectedMovie, setSelectedMovie ] = useState(null);
-
     if (selectedMovie) {
+        const relatedMovies = findRelatedMovies(movies, selectedMovie);
         return (
-        <MovieView key={selectedMovie._id} movie={selectedMovie} onBackClick={()=>setSelectedMovie(null)}/>
+        <div>
+            <MovieView key={selectedMovie._id} movie={selectedMovie} onBackClick={()=>setSelectedMovie(null)}/>
+            <br />
+            <h2>Related Movies</h2>
+            {relatedMovies.map( movie => (
+                <MovieCard key={movie._id} movie={movie} onCardClick={()=>setSelectedMovie(movie)}/>
+                )
+            )            }
+        </div>
         )
     }
 
@@ -37,3 +46,4 @@ const MainView = () => {
 }
 
 export default MainView;
+
